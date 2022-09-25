c327d84ced987bd8e35abc3bd28f2173d90d9bfd
Steele <shrevesa@mail.uc.edu>
2022-09-25 13:19:47 -0400

    Create fetchMessages function

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,3 +3,15 @@ const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
 const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
+
+function updateMessagesInChatBox() {
+  // Fetch Messages
+  fetchMessages();
+  // Loop over the messages
+
+}
+
+function fetchMessages() {
+    return fetch(serverURL)
+        .then( response => response.json())
+}
