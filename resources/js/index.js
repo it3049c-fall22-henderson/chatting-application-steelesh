66eb91aa15958f59efd25d0eb55a91b165e964c9
Steele <shrevesa@mail.uc.edu>
2022-09-25 13:18:07 -0400

    Create updateMessagesInChatBox function

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,3 +2,4 @@ const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
